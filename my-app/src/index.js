af107089b3f11cb2dcfb4c1c5c7c89aaa71359ae
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-11-12 09:55:50 +0000

    players take turns

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,32 +2,34 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-render () {
+function Square(props) {
     return (
         <button 
         className="square" 
-        onClick={() => this.props.onClick()}
+        onClick={props.onClick()}
         >
-        {this.props.value}
+        {props.value}
         </button>
         );
 }
-}
+
 
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
     
     handleClick(i) {
         const squares = this.state.squares.slice();
-        // slice creates a copy of the original array so we do not change the original.
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
     
     renderSquare(i) {
@@ -38,7 +40,7 @@ class Board extends React.Component {
     }
     
     render() {
-        const status="Next player: X";
+        const status="Next player: " + (this.state.xIsNext? 'X' : 'O');
         return (
             <div>
             <div className="status">{status}</div>
